4d8b934dd889f8edc17ad1f3f4cd5afa27e71468
Paul <validateus@yahoo.com>
2019-10-05 16:35:15 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+
+export interface Post {
+  title: string
+  text: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pipes';
+
+  // e: number = Math.E
+  //
+  // str = 'hello world'
+  //
+  // date: Date = new Date()
+  //
+  // float = 0.42
+  //
+  // obj = {
+  //   a: 1,
+  //   b: {
+  //     c: 2,
+  //     d: {
+  //       e: 3,
+  //       f: 4
+  //     }
+  //   }
+  // }
+
+  // searchField = 'title'
+  //
+  // search = ''
+  //
+  // posts: Post[] = [
+  //   {title: 'Beer', text: 'Самое лучшее пиво в мире'},
+  //   {title: 'Bread', text: 'The best bread in the world'},
+  //   {title: 'JavaScript', text: 'The best language in the world'}
+  // ];
+  //
+  // addPost() {
+  //   this.posts.unshift({
+  //     title: 'Rome',
+  //     text: 'Rome is greet!'
+  //   })
+  // }
+
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolved')
+    }, 4000)
+  })
+
+  date: Observable<Date> = new Observable(obs => {
+    setInterval(() =>{
+      obs.next(new Date())
+    }, 1000)
+  })
 }
